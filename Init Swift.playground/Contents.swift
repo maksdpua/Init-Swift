a4b41b84624633abaaf55e520b9323b50e5fa918
Maks <maksdpua@gmail.com>
2016-05-12 17:03:47 +0300

    Final

--- Init Swift.playground/Contents.swift
+++ Init Swift.playground/Contents.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 class Student1 {
     var firstName : String
     var lastName : String
@@ -90,6 +92,10 @@ class Human {
     func test() {
         
     }
+    
+    deinit {
+        print("Human deinitialized")
+    }
 }
 
 let h1 = Human(weight: 70, age: 25)
@@ -120,6 +126,8 @@ class Student: Human {
         self.weight = 50
         test()
     }
+    
+    
 }
 
 let student = Student(firstName: "a", lastName: "2")
@@ -156,10 +164,124 @@ class Doctor: Student {
 let stu1 = Student()
 
 let d1 = Doctor(firstName: "aaa")
-
 d1.age
 
 print(d1.age)
 
+////////////////////
 
-//let d2 = Doctor(
+enum Color : Int {
+    case Black
+    case White
+    
+    init?(_ value : Int) {
+        switch  value {
+        case 0:
+            self = Black
+        case 1:
+            self = White
+        default:
+            return nil
+        }
+    }
+}
+
+let a = Color(-1)
+//a!.rawValue
+
+let b = Color(rawValue: 0)
+
+struct  Size {
+    var width : Int
+    var height : Int
+    
+    init?(width: Int, height: Int) {
+        self.width = width
+        if  height < 0 {
+            return nil
+        }
+        self.height = height
+    }
+}
+
+
+class Friend : Human {
+    
+    var name : String!
+    
+    let skin : Color = {
+        let random = Int(arc4random_uniform(2))
+        return Color(random)!
+    }()
+    
+    init?(name: String) {
+        self.name = name
+        
+        super.init(weight: 0, age: 0)
+        if name.isEmpty {
+            return nil
+        }
+        
+    }
+    
+    required init() {
+        self.name = "Hi"
+        super.init(weight: 0, age: 0)
+    }
+    
+    deinit {
+        print("Friend deinitialized")
+    }
+}
+
+let f = Friend(name: "0")
+f!.name
+
+class BestFriend : Friend {
+    
+    
+    override init(name: String) {
+        
+        if name.isEmpty {
+            super.init()
+        } else {
+            super.init(name: name)!
+        }
+        
+        
+    }
+    
+    required init() {
+        fatalError("init() has not been implemented")
+    }
+    
+    deinit {
+        print("BestFriend deintitialized")
+    }
+    
+}
+
+let bf = BestFriend(name: "j")
+
+let f1 = Friend(name: "a")
+f1!.skin.rawValue
+
+let f2 = Friend(name: "b")
+f2!.skin.rawValue
+
+let f3 = Friend(name: "c")
+f3!.skin.rawValue
+
+let f4 = Friend(name: "d")
+f4!.skin.rawValue
+
+struct Test {
+    var bestFriend : BestFriend? = BestFriend(name: "")
+}
+
+var test : Test? = Test()
+
+test!.bestFriend = nil
+
+var f5 = Friend(name: "")
+
